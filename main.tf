b16ce494c06946bb8b2cfffed5165a06ca08ee61
jslopes <jslopes@uoldiveo.com>
2020-09-22 09:06:22 -0300

    first commit

---
+++ main.tf
@@ -0,0 +1,68 @@
+resource "random_id" "snapshot_identifier" {
+    count = var.create ? 1 : 0
+
+    byte_length = 4
+    keepers = {
+        id = var.docdb_name
+    }
+}
+resource "aws_docdb_cluster_instance" "main" {
+    count              = var.create ? var.instance_count : 0
+
+    identifier          = "${var.docdb_name}-instance-${count.index}"
+    cluster_identifier  = aws_docdb_cluster.main.0.id
+    instance_class      = var.instance_class
+    apply_immediately   = var.apply_immediately
+    engine              = var.engine
+    promotion_tier      = var.promotion_tier
+
+    tags = var.default_tags
+}
+resource "aws_docdb_cluster" "main" {
+    count = var.create ? 1 : 0
+
+    cluster_identifier      = var.docdb_name
+    engine                  = var.engine
+    engine_version          = var.engine_version
+    master_username         = var.master_username
+    master_password         = var.master_password
+    backup_retention_period = var.backup_retention_period
+    preferred_backup_window = var.preferred_backup_window
+    skip_final_snapshot     = var.skip_final_snapshot
+    vpc_security_group_ids  = var.vpc_security_group_ids 
+
+    db_subnet_group_name            = aws_docdb_subnet_group.main.0.id
+    db_cluster_parameter_group_name = aws_docdb_cluster_parameter_group.main.0.id
+
+    final_snapshot_identifier = "${lower(replace(var.docdb_name, " ", "-"))}-${random_id.snapshot_identifier.0.hex}-final-snapshot"
+
+    tags = var.default_tags
+}
+resource "aws_docdb_subnet_group" "main" {
+    count = var.create ? length(var.docdb_subnet_group) : 0
+
+    name       = lookup(var.db_subnet_group[count.index], "name", null)
+    subnet_ids = lookup(var.db_subnet_group[count.index], "subnet_ids", null)
+
+    tags = merge(
+        {
+            "Name" = "${format("%s", var.cluster_name)}-Subnet-Group"
+        },
+        var.default_tags,
+    )
+}
+resource "aws_docdb_cluster_parameter_group" "main" {
+    count = var.create ? length(var.docdb_parameter_group) : 0
+
+    name   = lookup(var.parameter_group.value, "name", null)
+    family = lookup(var.parameter_group.value, "family", null)
+
+    dynamic "parameter" {
+        for_each = lookup(var.parameter_group.value, "parameter", null)
+        content {
+            apply_method    = lookup(parameter.value, "name", null)
+            name            = lookup(parameter.value, "name", null)
+            value           = lookup(parameter.value, "value", null)
+        }
+    }
+}
---
+++ variables.tf
@@ -0,0 +1,64 @@
+variable "create" {
+    type = bool
+    default = true
+}
+variable "docdb_name" {
+    type = string
+}
+variable "engine" {
+    type = string
+    default = "docdb"
+}
+variable "master_username" {
+    type = string
+}
+variable "master_password" {
+    type = string
+}
+variable "backup_retention_period" {
+    type = number
+    default = "7"
+}
+variable "preferred_backup_window" {
+    type = string
+    default = "07:00-09:00"
+}
+variable "skip_final_snapshot" {
+    type = bool
+    default = true
+}
+variable "vpc_security_group_ids" {
+    type = list
+    default = []
+}
+variable "engine_version" {
+    type = string
+    default = ""
+}
+variable "docdb_subnet_group" {
+    type = any
+    default = []
+}
+variable "docdb_parameter_group" {
+    type = any
+    default = []
+}
+variable "default_tags" {
+    type = list(map)
+    default = {}
+}
+variable "apply_immediately" {
+    type = bool
+    default = false
+}
+variable "promotion_tier" {
+    type = number
+    default = "0"
+}
+variable "instance_count" {
+    type = number
+    default = "1"
+}
+variable "instance_class" {
+    type = string
+}
